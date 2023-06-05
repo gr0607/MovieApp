6ff81a6e80057f330f306142ab622913a2e438f9
gR <work.off.on@gmail.com>
2023-06-05 18:32:51 +0300

    prepare for coding

---
+++ MovieApp/App/MainTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabBarController.swift
+//  MovieApp
+//
+//  Created by admin on 05.06.2023.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
